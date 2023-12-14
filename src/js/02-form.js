0e1db3d71aa560255ce266365f395167ba33a6a9
AiHikki <freeomareps@gmail.com>
2023-12-14 22:34:46 +0200

    task 2

---
+++ src/js/02-form.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('.feedback-form');
+
+const data = JSON.parse(localStorage.getItem('feedback-form-state')) ?? {};
+
+form.elements.email.value = data.email ?? '';
+form.elements.message.value = data.message ?? '';
+
+form.addEventListener('input', handleInput);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (data.email && data.message) {
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+    console.log(data);
+  } else {
+    alert('Fill all the fields');
+  }
+});
+
+function handleInput(e) {
+  const formData = new FormData(e.currentTarget);
+  formData.forEach((value, key) => (data[key] = value.trim()));
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
